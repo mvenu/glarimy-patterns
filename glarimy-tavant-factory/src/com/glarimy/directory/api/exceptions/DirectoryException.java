7d6cd133e8b7b7cb334107e32158757d351f9232
Krishna Mohan Koyya <glarimy@Glarimy.local>
2018-04-10 15:08:04 +0530

    factory illustration for tavant

---
+++ glarimy-tavant-factory/src/com/glarimy/directory/api/exceptions/DirectoryException.java
@@ -0,0 +1,29 @@
+package com.glarimy.directory.api.exceptions;
+
+public class DirectoryException extends RuntimeException {
+
+	public DirectoryException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public DirectoryException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public DirectoryException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public DirectoryException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public DirectoryException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
